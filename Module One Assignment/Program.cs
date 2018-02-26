c45faac84a1c908a54498e728e305a2bf4d25f52
Jon Barrett <jbarrett32708@gmail.com>
2018-02-26 11:49:09 -0800

    Initial commit of solution

---
+++ Module One Assignment/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_One_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// initialize student variables
+            string studentFirstName = "";
+            string studentLastName = "";
+            DateTime studentBirthDate;
+            string studentAdd1 = "";
+            string studentAdd2 = "";
+            string studentCity = "";
+            string studentState = "";
+            int studentZipPostal;
+            string studentCountry = "";
+
+            // initialize teacher variables
+            string teacherFirstName = "";
+            string teacherLastName = "";
+            DateTime teacherBirthDate;
+            string teacherAdd1 = "";
+            string teacherAdd2 = "";
+            string teacherCity = "";
+            string teacherState = "";
+            int teacherZipPostal;
+            string teacherCountry = "";
+
+            // initialize UProgram variables
+            string programName = "";
+            string programDH = "";
+            string programDegreeOffered = "";
+
+            // initialize Degree variables
+            string degreeName = "";
+            int degreeCreditReq;
+
+            // initialize Course Info variables
+            string courseName = "";
+            int courseCredit;
+            int courseWeeks;
+            string courseTeacher = "";
+            
+            // create Console.Readline(); prompts for each variable
+            // ...
+
+
+
+        }
+    }
+}
